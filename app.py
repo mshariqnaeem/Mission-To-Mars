43d16e834e5cf71b6b33e97ecaaa9d205a617d63
mshariqnaeem <mshariqnaeem@gmail.com>
2022-01-16 17:26:07 -0500

    adding scraping and app .py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#Setup route for HTML page
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
